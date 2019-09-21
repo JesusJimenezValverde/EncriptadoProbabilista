6a319b93d55ddae0ff158c681b0bdcaf0dc4e1b8
Antony Artavia Palma <antonyartavia59@gmail.com>
2019-09-21 13:10:12 -0600

    Update Developed_Antony
    Changes in my compiler, because it was an old version

--- Encriptacion_Probabilista/src/Principal/principal.java
+++ Encriptacion_Probabilista/src/Principal/principal.java
@@ -3,7 +3,7 @@ package Principal;
 public class principal {
 
 	public static void main(String[] args) {
-		String[] letrasconB = new String[]{"a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z",};
+		String[] letrasconB = new String[]{"a","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z",};
 		 //String[] letras = new String[]{"a","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z",};
 		// TODO Auto-generated method stub
 		String toDecrypt = "xZwM7BWIpSjYyGFr9rhpEa+cYVtACW7yQKmyN6OYSCv0ZEg9jWbc6lKzzCxRSSIvOvlimQZBMZOYnOwiA9yy3YU8zk4abFSItoW6Wj0ufQ0=";
