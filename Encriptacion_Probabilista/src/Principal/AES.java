e471288408b1647f92f06d368ac1b64a0375c673
Antony Artavia Palma <antonyartavia59@gmail.com>
2019-09-23 16:44:10 -0600

    Update AES.java and Principal.java
    Just for now the code is not very elegant

--- Encriptacion_Probabilista/src/Principal/AES.java
+++ Encriptacion_Probabilista/src/Principal/AES.java
@@ -13,7 +13,19 @@ public class AES {
  
     private static SecretKeySpec secretKey;
     private static byte[] key;
+    public int logro=0;
+    public int isMenorMitad = 0;
+    private double[] probabilidades = {1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0};
  
+    
+    public double[] getProbabilidates() {
+    	return probabilidades;
+    }
+    
+    public void setProbabilidates(double[] pProbabilidades) {
+    	probabilidades = pProbabilidades;
+    }
+    
     public static void setKey(String myKey)
     {
         MessageDigest sha = null;
@@ -23,6 +35,7 @@ public class AES {
             key = sha.digest(key);
             key = Arrays.copyOf(key, 16);
             secretKey = new SecretKeySpec(key, "AES");
+            
         }
         catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
@@ -32,7 +45,7 @@ public class AES {
         }
     }
  
-    public static String encrypt(String strToEncrypt, String secret)
+    public String encrypt(String strToEncrypt, String secret)
     {
         try
         {
@@ -48,7 +61,7 @@ public class AES {
         return null;
     }
  
-    public static String decrypt(String strToDecrypt, String secret)
+    public String decrypt(String strToDecrypt, String secret)
     {
         try
         {
@@ -64,21 +77,105 @@ public class AES {
         return null;
     }
     
-    public static String pruebas(String strToDecrypt, String key, String[] letras) {
-
+    /**
+     * Function to prove the functionality
+     * 
+     * @param strToDecrypt is the encrypted String to decode
+     * @param key is the key that decode the strToDecrypt
+     * @param letras these are the 
+     * @return
+     */
+    public String Aprendisaje(String strToDecrypt, String key, String[] letras) {
+    	double miProb;
+    	int it = 0;
+    	double[] probabilidadesActuales = new double[26];
+    	
     	for(int letra = 0; letra < letras.length ; letra++) {
-    		for(int numero = 0; numero<10; numero++) {
+    		it++;
+    		
+    		miProb = generarProbabilidad();
+    		probabilidadesActuales[letra] = miProb;
+    		
+    		if(probabilidadesActuales[letra] < 0.5)
+    			continue;		
+    		
+    		for(int numero = 0; numero < 10; numero++) {
+    			it++;
     			String actual = key;
     			actual = key.replaceFirst("_", letras[letra]);
-    			System.out.println("Llave actual: "+actual);
+    			//System.out.println("Llave actual: "+actual);
     			actual = actual.replace("_", Integer.toString(numero));
-    			System.out.println("Llave actual: "+actual);
+    			//System.out.println("Llave actual: "+actual);
     			String nuevo = decrypt(strToDecrypt, actual);
+    			
     			if(nuevo != null) {
+    				aprender(probabilidadesActuales);
+    				System.out.println("Iteraciones: " + it);
+    				if(it <= 130)
+    					isMenorMitad++;
+    				logro++;
     				return nuevo;
     			}
     		}
     	}
     	return null;
+    }
+    
+    
+    public String prueba(String strToDecrypt, String key, String[] letras, double[] prop) {
+    	int it = 0;
+    	setProbabilidates(prop);
+    	
+    	for(int letra = 0; letra < letras.length ; letra++) {
+    		it++;
+    		
+    		if(probabilidades[letra] < 0.5)
+    			continue;		
+    		
+    		for(int numero = 0; numero < 10; numero++) {
+    			it++;
+    			String actual = key;
+    			actual = key.replaceFirst("_", letras[letra]);
+    			//System.out.println("Llave actual: "+actual);
+    			actual = actual.replace("_", Integer.toString(numero));
+    			//System.out.println("Llave actual: "+actual);
+    			String nuevo = decrypt(strToDecrypt, actual);
+    			
+    			if(nuevo != null) {
+    				System.out.println("Iteraciones: " + it);
+    				if(it <= 130)
+    					isMenorMitad++;
+    				logro++;
+    				return nuevo;
+    			}
+    		}
+    	}
+    	return null;
+    }
+    
+    /**
+     * This function generate a random double
+     * 
+     * @return a double, that is a probability between 0.0 and 1.0
+     */
+    public static double  generarProbabilidad() {
+		double num1 = (double) (Math.random() * 10) + 1;
+		double num2 = (double) (Math.random() * 10) + num1;
+		
+		return num1/num2;
+	}
+    
+    /**
+     * 
+     * 
+     * @param probabilidaesNuevas
+     */
+    public void aprender(double[] probabilidaesNuevas) {
+    	for(int index=0; index<26; index++) {
+    		if(probabilidaesNuevas[index] < 0.5) {
+    			//System.out.println("Probabilidad pos:"+index+" = "+probabilidaesNuevas[index]);
+    			probabilidades[index] = probabilidaesNuevas[index];
+    		}
+    	}
     }
 }
--- Encriptacion_Probabilista/src/Principal/principal.java
+++ Encriptacion_Probabilista/src/Principal/principal.java
@@ -1,16 +1,41 @@
 package Principal;
 
+import java.io.IOException;
+
 public class principal {
 
-	public static void main(String[] args) {
-		String[] letrasconB = new String[]{"a","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z",};
-		 //String[] letras = new String[]{"a","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z",};
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {	
+		String[] letrasconB = new String[]{"a","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z"};
+		
 		String toDecrypt = "xZwM7BWIpSjYyGFr9rhpEa+cYVtACW7yQKmyN6OYSCv0ZEg9jWbc6lKzzCxRSSIvOvlimQZBMZOYnOwiA9yy3YU8zk4abFSItoW6Wj0ufQ0=";
 		String secretKey = "29dh120_dk1_3";
 		
-		String resultado = AES.pruebas(toDecrypt, secretKey, letrasconB);
-		System.out.println(resultado);
+		AES aes = new AES();
+		
+		for(int i=0; i<100; i++) {
+			String resultado = aes.Aprendisaje(toDecrypt, secretKey, letrasconB);
+			System.out.println(resultado);
+		}
+		
+		System.out.println();
+		System.out.println();
+		System.out.println("Cantidad de logro: "+aes.logro);
+		System.out.println("Cant de menorMitad iteraciones: "+aes.isMenorMitad);
+		
+		for(int i=0; i<letrasconB.length; i++) {
+			System.out.println(letrasconB[i]+": " + aes.getProbabilidates()[i]);
+		}
+		
+		System.out.println();
+		System.out.println();
+		AES aes2 = new AES();
+		for(int i=0; i<100; i++) {
+			String resultado = aes2.prueba(toDecrypt, secretKey, letrasconB, aes.getProbabilidates());
+			System.out.println(resultado);
+		}
+
+		System.out.println("Cantidad de logro: "+aes2.logro);
+		System.out.println("Cant de menorMitad iteraciones: "+aes2.isMenorMitad);
 	}
 
 }
